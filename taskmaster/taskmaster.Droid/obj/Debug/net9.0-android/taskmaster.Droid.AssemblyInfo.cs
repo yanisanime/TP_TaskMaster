f91ca806f3412d20a11bfcca1e6649bf958fd9bc
yanis <meriem.yanis05@gmail.com>
2025-04-08 16:57:56 +0200

    version de base avec MAUI

--- taskmaster/taskmaster.Droid/obj/Debug/net9.0-android/taskmaster.Droid.AssemblyInfo.cs
+++ taskmaster/taskmaster.Droid/obj/Debug/net9.0-android/taskmaster.Droid.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taskmaster.Droid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b46baad8faa9c4dedfd9d0f34848350391f063")]
 [assembly: System.Reflection.AssemblyProductAttribute("taskmaster.Droid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taskmaster.Droid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
