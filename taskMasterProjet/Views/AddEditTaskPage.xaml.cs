06dd25d2c8ed81626d04be61afaddea240dadd6b
yanis <meriem.yanis05@gmail.com>
2025-04-11 17:31:39 +0200

    les commentaire s'affiches bien et s'enregistre dans la base

--- taskMasterProjet/Views/AddEditTaskPage.xaml.cs
+++ taskMasterProjet/Views/AddEditTaskPage.xaml.cs
@@ -1,11 +1,11 @@
-﻿using taskMasterProjet.ViewModels;
-
+﻿using Microsoft.Maui.Controls;
+using taskMasterProjet.ViewModels;
 
 namespace taskMasterProjet.Views;
 
-
-public partial class AddEditTaskPage :ContentPage
+public partial class AddEditTaskPage : ContentPage
 {
+    private List<Entry> _commentEntries = new();
 
     public AddEditTaskPage(AddEditTaskViewModel viewModel)
     {
@@ -13,7 +13,46 @@ public partial class AddEditTaskPage :ContentPage
         BindingContext = viewModel;
     }
 
+    private void OnAjouterCommentaireClicked(object sender, EventArgs e)
+    {
+        var entry = new Entry { Placeholder = "Écrire un commentaire..." };
+        _commentEntries.Add(entry);
+        CommentairesLayout.Children.Add(entry);
+    }
+
+    protected override async void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        // Passer les commentaires au ViewModel
+        if (BindingContext is AddEditTaskViewModel vm)
+        {
+            vm.CommentaireTextes = _commentEntries
+                .Where(e => !string.IsNullOrWhiteSpace(e.Text))
+                .Select(e => e.Text!)
+                .ToList();
+        }
+    }
+
+    public void TransferCommentairesToViewModel()
+    {
+        if (BindingContext is AddEditTaskViewModel vm)
+        {
+            vm.CommentaireTextes = _commentEntries
+                .Where(e => !string.IsNullOrWhiteSpace(e.Text))
+                .Select(e => e.Text!)
+                .ToList();
+        }
+    }
+
+    private async void OnSaveClicked(object sender, EventArgs e)
+    {
+        TransferCommentairesToViewModel();
+
+        if (BindingContext is AddEditTaskViewModel vm)
+        {
+            await vm.SaveCommand.ExecuteAsync(null);
+        }
+    }
+
 }
-
-
-
